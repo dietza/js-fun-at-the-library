e4f11c4fd51618e5255ed29fc43c067d3c85dcfe
Allison Dietz <dietz.allison@outlook.com>
2020-08-23 20:00:56 -0600

    Add book.js, book-test.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,55 @@
-function createTitle() {
-  
+function createTitle(title) {
+  var modifiedTitle = (`The ${title}`);
+  return modifiedTitle;
+}
+
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+  return character;
+}
+
+
+function saveReview(newReview,reviews) {
+  if (reviews.includes(newReview) == true) {
+    return reviews;
+  } else {
+  reviews.push(newReview);
+  return reviews;
+  };
+}
+
+
+function calculatePageCount(title) {
+  var pages = ((title.length) * 20);
+  return pages;
+}
+
+
+function writeBook(title, character, genre) {
+  var book = {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  };
+  return book;
+}
+
+
+function editBook(book) {
+book.pageCount = ((calculatePageCount(book.title)) * 0.75);
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
