2c84cca230e2fc8d4cd42fa86f1c45f49a96e1b0
Allison Dietz <dietz.allison@outlook.com>
2020-08-23 20:15:27 -0600

    Refactor book.js

--- src/book.js
+++ src/book.js
@@ -3,56 +3,32 @@ function createTitle(title) {
   return modifiedTitle;
 }
 
+
 function buildMainCharacter(name, age, pronouns) {
   var character = {
-    name: name,
-    age: age,
-    pronouns: pronouns
+    name,
+    age,
+    pronouns
   };
   return character;
 }
 
-/*
-Used above for more explicit clarity.
-Could REFACTOR buildMainCharacter to be:
 
-function buildMainCharacter(name, age, pronouns) {
-  var character = {
-    name,     <--   when paired keys and values are
-    age,      <--  the same, can used singularly.
-    pronouns      <--
-  };
-  return character;
-}
-*/
-
-
-function saveReview(newReview,reviewsArr) {
-  if (reviewsArr.includes(newReview) == true) {
-    return reviewsArr;
+function saveReview(newReview,reviews) {
+  if (reviews.includes(newReview) == true) {
+    return reviews;
   } else {
-  reviewsArr.push(newReview);
-  return reviewsArr;
+  reviews.push(newReview);
+  return reviews;
   };
 }
 
-/*
-REFACTOR:
-
-function saveReview(newReview,reviewsArr) {
-  if (!reviewsArr.includes(newReview)) {
-    reviewsArr.push(newReview)
-    return reviewsArr;
-  } else {
-  return reviewsArr;
-  };
-}
-*/
 
 function calculatePageCount(title) {
   var pages = ((title.length) * 20);
   return pages;
 }
+
 
 function writeBook(title, character, genre) {
   var book = {
@@ -64,25 +40,6 @@ function writeBook(title, character, genre) {
   return book;
 }
 
-/*
-REFACTOR (clarifying order of arguments for test2):
-
-function writeBook(title, character, genre, pages) {
-  var book = {
-    title: title,
-    mainCharacter: character,
-    pageCount: calculatePageCount(title),
-    genre: genre
-  };
-  return book;
-}
-
-Because the test files passes arguments as
-var book = writeBook(bookTitle, bookCharacter, "fantasy");
-and NOTHING for the pageCount (we are pulling a previously established function to create that value within the object),
-the string "fantasy" is the third argument passed,
-so genre must be our third parameter to establish that key-value pair.
-*/
 
 function editBook(book) {
 book.pageCount = ((calculatePageCount(book.title)) * 0.75);
